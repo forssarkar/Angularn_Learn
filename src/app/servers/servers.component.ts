5a63f4222666c0e1a35216cfe54193120245899e
forssarkar <forssarkar@gmail.com>
2022-08-14 18:20:46 +0530

    Lesson 4 : Data Binding (Property, Event, String Interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+   allowNewServer : boolean = false ;
+   serverCreationStatus : string = "No server created";
+   noOfServer : number = 0;
+   serverName = '';
 
-  constructor() { }
+  constructor() { 
+    setTimeout (() => {
+        this.allowNewServer= true
+    },2000)
+
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer = () => {
+    this.serverCreationStatus = "Server Created"
+    this.noOfServer =  this.noOfServer + 1
+  }
+
+  onUpdateServerName(event : Event){
+    //console.log( (<HTMLInputElement>event.target).value)
+    //console.log( event.target.value)
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
