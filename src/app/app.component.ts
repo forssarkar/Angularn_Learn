d54ffb5558b071aa599676be254525e5ec64d1ce
forssarkar <forssarkar@gmail.com>
2022-08-15 08:08:55 +0530

    Lesson 07 Assignment & Index ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'HMS_Angular';
   name = "Susanta" ;
+  showDetails = false;
+  logDetails :any[] = [];
     CallSomeLogic = () => {
     alert("hi")
   }
+
+  // onDetailsToggle =() => {
+  //   this.showDetails = !this.showDetails ;
+  //   this.logDetails.push(this.logDetails.length + 1); 
+  // }
+
+  // Another Version for Index
+    onDetailsToggle =() => {
+    this.showDetails = !this.showDetails ;
+    this.logDetails.push(new Date()); 
+  }
 }
+
